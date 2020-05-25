f2f147bb9a77a288e387e9fb05bd0091c6488815
Mugilan <mugilancodes@gmail.com>
2020-05-25 13:50:05 +0530

    Create Empty BrowserWindow

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 const { app, BrowserWindow } = require('electron');
+
+const createMainWindow = () => {
+  const mainWindow = new BrowserWindow({
+    title: 'ImageShrink',
+    width: 500,
+    height: 600,
+  });
+};
+
+app.on('ready', createMainWindow);
