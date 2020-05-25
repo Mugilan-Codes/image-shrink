63f70d5364dccce0caba5bd192940d0140dfe6cb
Mugilan <mugilancodes@gmail.com>
2020-05-25 14:12:44 +0530

    Create app folder

--- main.js
+++ main.js
@@ -9,7 +9,8 @@ const createMainWindow = () => {
     height: 600,
   });
 
-  mainWindow.loadURL('https://twitter.com');
+  mainWindow.loadURL(`file://${__dirname}/app/index.html`);
+  //   mainWindow.loadFile('./app/index.html');
 };
 
 app.on('ready', createMainWindow);
