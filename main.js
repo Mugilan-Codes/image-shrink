75e5eb015b2c8f449251d3c57b8f167f8eebce27
Mugilan <mugilancodes@gmail.com>
2020-05-26 19:04:26 +0530

    Check for Platform

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 const { app, BrowserWindow } = require('electron');
 
+// SET ENVIRONMENT
+process.env.NODE_ENV = 'development';
+
+const isDev = process.env.NODE_ENV !== 'production' ? true : false;
+const isMac = process.platform === 'darwin' ? true : false;
+
 let mainWindow;
 
 const createMainWindow = () => {
