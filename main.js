c885e74c49fd6939b4703aa4d3b9b090c7401bdf
Mugilan <mugilancodes@gmail.com>
2020-05-26 19:23:50 +0530

    Basic MacOS Functionality

--- main.js
+++ main.js
@@ -21,3 +21,20 @@ const createMainWindow = () => {
 };
 
 app.on('ready', createMainWindow);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (!isMac) {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
