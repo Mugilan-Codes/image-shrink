f71bfdd0bc82b68d7fd08d3c5ba632116836fe0f
Mugilan <mugilancodes@gmail.com>
2020-05-30 20:39:34 +0530

    Get Form Values

--- app/js/renderer.js
+++ app/js/renderer.js
@@ -1,6 +1,18 @@
 const path = require('path');
 const os = require('os');
 
-const outputPath__span = document.getElementById('output-path');
+const form = document.getElementById('image-form');
+const img = document.getElementById('img');
+const slider = document.getElementById('slider');
+const outputPath = document.getElementById('output-path');
 
-outputPath__span.innerText = path.join(os.homedir(), 'imageshrink');
+outputPath.innerText = path.join(os.homedir(), 'imageshrink');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const imgPath = img.files[0].path;
+  const imgQuality = slider.value;
+
+  console.log(imgPath, imgQuality);
+});
