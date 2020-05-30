c94981d8aebb4f453f2187ef9e3490c7088e0a3f
Mugilan <mugilancodes@gmail.com>
2020-05-30 20:15:39 +0530

    Display Output Path

--- app/js/renderer.js
+++ app/js/renderer.js
@@ -1,2 +1,6 @@
 const path = require('path');
 const os = require('os');
+
+const outputPath__span = document.getElementById('output-path');
+
+outputPath__span.innerText = path.join(os.homedir(), 'imageshrink');
